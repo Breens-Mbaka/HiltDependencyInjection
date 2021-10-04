d8abb636fbb8437063fbe7d5b4547839694dc77b
Breens-Thomas <mbakabreens@gmail.com>
2021-10-04 12:55:29 +0300

    Provides and Binds

--- app/src/main/java/com/example/hiltdependencyinjection/MainActivity.kt
+++ app/src/main/java/com/example/hiltdependencyinjection/MainActivity.kt
@@ -2,39 +2,78 @@ package com.example.hiltdependencyinjection
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
+import com.google.gson.Gson
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.scopes.ActivityScoped
-import dagger.hilt.android.scopes.FragmentScoped
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    //field injection
     @Inject
     lateinit var someClass: SomeClass
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         println(someClass.doAThing())
     }
 }
 
-@AndroidEntryPoint
-class MyFragment: Fragment() {
-    @Inject
-    lateinit var someClass: SomeClass
-}
-
-@ActivityScoped
 class SomeClass
 @Inject
-constructor() {
-
+constructor(
+    private val someInterfaceImpl: SomeInterface,
+    private val gson: Gson
+){
     fun doAThing(): String{
-        return "Look I did a thing!"
+        return "Look I got: ${someInterfaceImpl.getAThing()}"
+    }
+}
+
+class SomeInterfaceImpl
+@Inject
+constructor(
+    private val someDependency: String
+) : SomeInterface{
+    override fun getAThing(): String {
+        return "A thing, ${someDependency}"
+    }
+}
+
+interface SomeInterface {
+
+    fun getAThing(): String
+}
+
+@InstallIn(SingletonComponent::class)
+@Module
+class MyModule {
+    @Singleton
+    @Provides
+    fun provideSomeString(): String{
+        return "some string"
+    }
+
+    @Singleton
+    @Provides
+    fun provideSomeInterface(
+        someString: String
+    ): SomeInterface {
+        return SomeInterfaceImpl(someString)
+    }
+
+    @Singleton
+    @Provides
+    fun provideGson(): Gson{
+        return Gson()
     }
 }
